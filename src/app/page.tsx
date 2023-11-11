5b7f063100b40df7d4230de79c8ec49d8ef9bcda
Ferwin Lopez <ferwinlopezt@gmail.com>
2023-11-11 09:35:36 +0530

    nothing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,16 @@
 import Footer from '@/components/Footer'
 import NavBar from '../components/NavBar'
 import ParallaxText from '@/components/ParallaxText'
+import "./globals.css"
 
 export default function Home() {
   return (
     <main>
+    
       <NavBar></NavBar>
+      {/* <div className="spinner-wrapper">
+      <div className="spinner"></div>
+    </div> */}
       <h2 className='h-96'>This is the Home Page of Kerala Blasters💛⭐💛⭐💛</h2>
       <ParallaxText baseVelocity={-5} >Kerala Blasters</ParallaxText>
       <ParallaxText baseVelocity={5}>Football Club</ParallaxText>
--- src/components/NavList.tsx
+++ src/components/NavList.tsx
@@ -5,11 +5,11 @@ const NavList = () => {
   return (
     <div >
          <div className=" hidden md:flex flex-row gap-9 items-center">
-          <Link href="/" className="link">Home</Link>
-        <Link href="./team" className="link">Team</Link>
-        <Link href="./about" className="link">About</Link>
-        <Link href="./news" className="link">News</Link>
-        <Link href="./fans" className="link">Fans</Link>
+          <Link href="/" passHref className="link">Home</Link>
+        <Link href="./team" passHref className="link">Team</Link>
+        <Link href="./about" passHref className="link">About</Link>
+        <Link href="./news" passHref className="link">News</Link>
+        <Link href="./fans" passHref className="link">Fans</Link>
          </div>
         <div className="lg:hidden md:hidden  xl:hidden px-4">
         <RxHamburgerMenu size={30} className="text-black cursor-pointer" />
