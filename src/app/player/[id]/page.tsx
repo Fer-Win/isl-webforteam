4c7d63ca447f6b9384ee54bb91ca7ecc19c4a96a
Ferwin Lopez <ferwinlopezt@gmail.com>
2023-11-12 16:12:35 +0530

    individual player info fix

--- src/app/player/[id]/page.tsx
+++ src/app/player/[id]/page.tsx
@@ -13,17 +13,26 @@ const page = ({params}:any) => {
             return (
               <main className="flex h-[550px] my-10 mx-28" key={player.id}>
                  {/* <div className="title font-familjen font-bold text-[5rem] opacity-80">Player Info</div> */}
-                <div className="w-[60%] border-2 border-stone-50  ">
-                  <div className="">
-                    <div>{player.stats.position}</div>
+                <div className="w-[60%] border-2 border-r-0 border-stone-50 flex flex-col justify-evenly ">
+                  <div className="info-section">
                     <div>{player.name}</div>
                   </div>
-                  <div>{player.stats.nationality}</div>
-                  <div>{player.stats.age}</div>
-                  <div>{player.stats.height}</div>
+                  <div className="info-section">
+                  <p className="text-lg label">Country</p>
+                    {player.stats.nationality}
+                    </div>
+                  <div className="info-section">
+                  <p className="text-lg label">Position</p>
+                    {player.stats.position}</div>
+                  <div className="h-40 text-[55px] px-3 bg-slate-300 font-familjen flex flex-col justify-center">
+                  
+                <div className="flex flex-row  w-full justify-start items-center gap-20"><div><p className="text-lg label">Age</p>  {player.stats.age} </div>
+                <div className="w-[2px] bg-stone-50 h-[150px]"></div>
+                  <div><p className="text-lg label">Height</p>{player.stats.height}</div></div>
+                    </div>
                 </div>
-                <div className="w-[40%] bg-[#013ca6] border-2 border-stone-50 overflow-hidden relative " >
-                <img src="/kbfc_logo.svg" alt="" className="player-bg w-96 opacity-50" />
+                <div className="w-[40%] bg-[#013ca6] border-2 border-stone-50 overflow-hidden transition-all ease-in-out duration-500 hover:bg-[#fff212]  relative group " >
+                <img src="/kbfc_logo.svg" alt="" className="player-bg w-96 opacity-50 group-hover:scale-0 transition-all ease-in-out duration-300" />
                 <img src={player.image} alt="" className="z-40 absolute" />
                                  
                 </div>
