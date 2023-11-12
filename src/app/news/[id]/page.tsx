307b7360fa7d5c730da8213a5ba324ef31261c2e
Ferwin Lopez <ferwinlopezt@gmail.com>
2023-11-12 14:25:05 +0530

    fix individual news page

--- src/app/news/[id]/page.tsx
+++ src/app/news/[id]/page.tsx
@@ -13,13 +13,13 @@ const page = ({params}:any) => {
             return (
               <main className="flex flex-col text-white px-7 font-familjen" key={news.id}>
                 <div className="w-[80%] my-8 text-3xl ">{news.title}</div>
-                <div className="w-[80%] bg-[#013ca6] overflow-hidden relative " >
+                <div className="w-[80%] h-[400px] overflow-hidden  " >
       
-                <img src={news.url} alt="" className="z-40 absolute" />
+                <img src={news.url} alt="" className="z-40 w-[900px] rounded-sm my-4 object-contain" />
                                  
                 </div>
-                <div className="font-sans">{news.description}</div>
-                <div>{news.date}</div>
+                <div className="font-sans mt-8">{news.description}</div>
+                <div className="text-lg my-5">{news.date}</div>
                 
               </main>
             )
