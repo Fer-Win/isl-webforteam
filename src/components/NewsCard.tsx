d81f7c27ea7058c9799ba97ff083d9e841c18082
Ferwin Lopez <ferwinlopezt@gmail.com>
2023-11-13 07:11:08 +0530

    newscard responsive

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -10,14 +10,14 @@ interface NewsCardProps {
 
 const NewsCard: React.FC<NewsCardProps>=({id,title,cut,date,image})=>{
   return (
-    <a href={`/news/${id}`} className=" text-white flex justify-center items-center  py-6 rounded-sm gap-7 hover:bg-[#013ca6] hover:bg-opacity-20 transition-all duration-100 group ease-in">
-      <div className="w-3/5">
-      <h1 className="text-2xl font-familjen font-bold mb-3">{title}</h1>
-      <p className="opacity-80">{cut}</p>
-      <h3 className="text-lg opacity-80  font-familjen pt-3">{date}</h3>
+    <a href={`/news/${id}`} className=" text-white flex flex-col sm:flex-row justify-center items-center  py-6 rounded-sm gap-7 hover:bg-[#013ca6] hover:bg-opacity-20 transition-all duration-100 group ease-in">
+      <div className="w-[95%] px-5 sm:w-3/5">
+          <h1 className="sm:text-2xl text-sm w-full font-familjen font-bold mb-3">{title}</h1>
+          <p className="opacity-80 overflow-clip sm:overflow-auto h-[100px] sm:h-auto  ">{cut}</p>
+          <h3 className="sm:text-lg text-sm opacity-80  font-familjen pt-1 sm:pt-3">{date}</h3>
       </div>
       <div>
-        <img src={image} alt="" className="w-[350px]  object-cover  group-hover:rounded-lg group-hover:scale-105 transition-all duration-100 ease-in"/>
+        <img src={image} alt="" className="sm:w-[350px] w-[250px] rounded-[1px] sm:rounded-none object-cover  group-hover:rounded-lg group-hover:scale-105 transition-all duration-100 ease-in"/>
       </div>
     </a>
   )
