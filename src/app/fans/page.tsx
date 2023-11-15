c3be8b5b0192d6102b138443ae3042298b32de6f
Ferwin Lopez <ferwinlopezt@gmail.com>
2023-11-15 07:08:38 +0530

    content fix

--- src/app/fans/page.tsx
+++ src/app/fans/page.tsx
@@ -5,28 +5,10 @@ import NavBar from "../../components/NavBar"
 import ParallaxText from "@/components/ParallaxText"
 import React from "react";
 import DemoCarousel from "@/components/Swiper";
-// import Slider from "react-slick";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import {useState,useEffect} from 'react'
+
 
 const Fans = () => {
-  const [slidesPerView, setSlidesPerView] = useState(1);
 
-  useEffect(() => {
-    const updateSlidesPerView:any = () => {
-      if (window.innerWidth <= 768) {
-        setSlidesPerView(1);
-      } else {
-        setSlidesPerView(4); 
-      }
-    };
-    updateSlidesPerView();
-
-    window.addEventListener('resize', updateSlidesPerView);
-    return () => {
-      window.removeEventListener('resize', updateSlidesPerView);
-    };
-  }, []);
 
 
   return (
@@ -41,7 +23,7 @@ const Fans = () => {
     <ParallaxText baseVelocity={-5} >#YENNUM YELLOW</ParallaxText>
       <ParallaxText baseVelocity={5}>#MANJAPPADA</ParallaxText>
       <div className='font-sans mt-1 text-white text-sm sm:text-2xl font-light ml-4 sm:ml-8 sm:mt-4'>
-      Thank you for being a part of the Kerala Blasters family. <br /> Let's roar together, because <span className='text-[#fff212] font-roadRage'>#YennumYellow</span>
+      Thank you for being a part of the Kerala Blasters family. <br /> Let&apos;s roar together, because <span className='text-[#fff212] font-roadRage'>#YennumYellow</span>
       </div>
       <Footer></Footer>
       </main>
